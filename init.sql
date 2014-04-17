d8054c2d0dd4c9b6a71d8f84a556db9d1c05bdc4
Alexander Selzer <aselzer3@gmail.com>
2014-04-17 13:18:27 +0200

    Data insertion

---
+++ init.sql
@@ -0,0 +1,19 @@
+create table diary_survey (
+  gender VARCHAR,
+  grade VARCHAR,
+  usage VARCHAR,
+  more_space VARCHAR,
+  cover VARCHAR,
+  size VARCHAR,
+  layout VARCHAR,
+  plastic_triangle VARCHAR,
+  info VARCHAR,
+  timestamp VARCHAR,
+  sketching VARCHAR,
+  online_service VARCHAR,
+  use VARCHAR,
+  looks VARCHAR,
+  quotes VARCHAR,
+  map VARCHAR,
+  other_suggestions VARCHAR(2048)
+);
